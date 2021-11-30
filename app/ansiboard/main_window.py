96ded173e9f9bc048534e2cffc890e13fec28f9b
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-11-30 06:08:02 -0500

    Actual drawing works now

--- app/ansiboard/main_window.py
+++ app/ansiboard/main_window.py
@@ -3,6 +3,71 @@ import qtawesome as qta
 from ansiboard.network import NetworkStatusWidget
 import json
 
+
+class Stroke:
+    def __init__(self):
+        self.points = []
+
+    def add_point(self, point):
+        self.points.append(point)
+
+
+class Canvas(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        
+        self.client = QApplication.instance().client
+        self.server =  QApplication.instance().server
+        self.server.message_received.connect(self.receive_data)
+
+        self.strokes = []
+        self.stroke = None
+
+    def add_stroke(self, stroke):
+        self.strokes.append(stroke)
+    
+    def paintEvent(self, event):
+        p = QPainter()
+        p.begin(self)
+        for s in self.strokes:
+            p.drawPath(s)
+
+        if self.stroke:
+            p.drawPath(self.stroke)
+        p.end()
+
+    def tabletEvent(self, event):
+        data = {}
+        event.accept()
+        if event.type() == QEvent.TabletPress:
+            data['event'] = 'press'
+            data['type'] = event.pointerType()
+        elif event.type() == QEvent.TabletMove:
+            data['event'] = 'move'
+        elif event.type() == QEvent.TabletRelease:
+            data['event'] = 'release'
+
+        data['pos'] = event.posF()
+        self.receive_data(data)
+        self.client.send_message(json.dumps(data))
+
+    def receive_data(self, data):
+        if data['event'] == 'press':
+            if data['type'] == QTabletEvent.PointerType.Pen:
+                self.stroke = QPainterPath()
+                self.stroke.moveTo(data['pos'])
+                self.update()
+        if data['event'] == 'move':
+            if self.stroke:
+                self.stroke.lineTo(data['pos'])
+                self.update()
+        if data['event'] == 'release':
+            if self.stroke:
+                self.add_stroke(self.stroke)
+                self.stroke = None
+                self.update()
+
+
 class MainWindow(BaseMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
@@ -10,25 +75,8 @@ class MainWindow(BaseMainWindow):
         self.installEventFilter(self)
         self.network_status = NetworkStatusWidget(self)
 
-        self.client = QApplication.instance().client
-        self.server =  QApplication.instance().server
-        self.server.message_received.connect(self.event_recieved)
-
-        self.pos = QLabel()
-        self.xtilt = QLabel()
-        self.ytilt = QLabel()
-        self.pressure = QLabel()
-        self.pointer = QLabel()
-
-        with CVBoxLayout(self) as layout:
-            layout.add(self.pos)
-            with layout.hbox():
-                layout.add(self.xtilt)
-                layout.add(self.ytilt)
-                layout.add(QLabel(), 1)
-            layout.add(self.pressure)
-            layout.add(self.pointer)
-            layout.add(QLabel(), 1)
+        self.canvas = Canvas()
+        self.setCentralWidget(self.canvas)
 
         self.init_statusbar()
         
@@ -38,22 +86,6 @@ class MainWindow(BaseMainWindow):
         
         self.status.add_spacer()
         self.status.addWidget(self.network_status)
-
-    def tabletEvent(self, event):
-        event.accept()
-        print(type(event))
-        # if event.type() == QEvent.TabletPress:
-        # elif event.type() == QEvent.TabletRelease:
-        # elif event.type() == QEvent.TabletMove:
-        d = {
-            'posF': str(event.posF()),
-            'xTilt': str(event.xTilt()),
-            'yTilt': str(event.yTilt()),
-            'pressure': str(event.pressure()),
-            'pointerType': str(event.pointerType()),
-        }
-        self.event_recieved(d)
-        self.client.send_message(json.dumps(d))
 
     def event_recieved(self, msg):
         try:
