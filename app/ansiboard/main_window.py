075ccbb3382d758ef275693842393fcfb52179d3
DaelonSuzuka <daelonsuzuka@gmail.com>
2021-11-30 03:37:28 -0500

    Added debug information

--- app/ansiboard/main_window.py
+++ app/ansiboard/main_window.py
@@ -6,9 +6,25 @@ from ansiboard.network import NetworkStatusWidget
 class MainWindow(BaseMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-
-        # self.setCentralWidget()
+        self.setAttribute(Qt.WA_AcceptTouchEvents, True)
+        
         self.network_status = NetworkStatusWidget(self)
+
+        self.pos = QLabel()
+        self.xtilt = QLabel()
+        self.ytilt = QLabel()
+        self.pressure = QLabel()
+        self.pointer = QLabel()
+
+        with CVBoxLayout(self) as layout:
+            layout.add(self.pos)
+            with layout.hbox():
+                layout.add(self.xtilt)
+                layout.add(self.ytilt)
+                layout.add(QLabel(), 1)
+            layout.add(self.pressure)
+            layout.add(self.pointer)
+            layout.add(QLabel(), 1)
 
         self.init_statusbar()
         
@@ -20,3 +36,13 @@ class MainWindow(BaseMainWindow):
         
 
         self.status.addWidget(self.network_status)
+
+    def tabletEvent(self, event):
+        self.pos.setText(str(event.posF()))
+        self.xtilt.setText(str(event.xTilt()))
+        self.ytilt.setText(str(event.yTilt()))
+        self.pressure.setText(str(event.pressure()))
+        self.pointer.setText(str(event.pointerType()))
+
+    def touchEvent(self, event):
+        print(event)
